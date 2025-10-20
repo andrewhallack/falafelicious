286d4a3cbd48f831404c14a2a398e6b777014cc2
andrewhallack <andrewhallack@gmail.com>
2025-10-20 02:13:25 -0500

    added nutrition section

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <HealthIcons />
       <Intro />
       <WhatsInside />
+      <NutritionFacts />
     </>
   )
 }
--- src/components/NutritionFacts/NutritionFacts.jsx
+++ src/components/NutritionFacts/NutritionFacts.jsx
@@ -1,25 +1,32 @@
 import './nutrition-facts.css'
-import nutrition from '../../assets/nutrition-facts.png'
-
 
 const NutritionFacts = () => {
   return (
-    <div className='nutrition-facts'>
-        <div className='moving-words'>
-            <h1>WHOLESOME INGREDIENTS, NOTHING ELSE.</h1>
-            <h1>WHOLESOME INGREDIENTS, NOTHING ELSE.</h1>
+    <section className='nutrition-facts'>
+        <div className='info'>
+            <h3>Wholesome, Simple, Delicious</h3>
+            <p>
+                Each bag of Falafelicious contains 12 crispy falafel balls made from chickpeas, parsley,
+                onion, garlic, and spices. That’s it — clean ingredients you can trust.
+            </p>
         </div>
-        <div className='info-wrapper'>
-            <div className='info'>
-                <h3>Wholesome, Simple, Delicious</h3>
-                <p>
-                    Each bag of Falafelicious contains 12 crispy falafel balls made from chickpeas, parsley,
-                    onion, garlic, and spices. That’s it — clean ingredients you can trust.
-                </p>
-            </div>
-            <img src={nutrition} />
+        <div className='black'>
+            <h3>13g</h3>
+            <h4>PROTEIN PER SERVING</h4>
         </div>
-    </div>
+        <div className='green'>
+            <h3>9g</h3>
+            <h4>FIBER PER SERVING</h4>
+        </div>
+        <div className='red'>
+            <h3>260</h3>
+            <h4>CALORIES PER SERVING</h4>
+        </div>
+        <div className='brown'>
+            <h3>260</h3>
+            <h4>DELICIOUS</h4>
+        </div>
+    </section>
   )
 }
 
