454a4767d51a4190151e1d462a330ab760f6c80e
andrewhallack <andrewhallack@gmail.com>
2025-10-29 11:51:10 -0500

    Small map adjustments

--- src/components/FindInStores/FindInStores.jsx
+++ src/components/FindInStores/FindInStores.jsx
@@ -3,7 +3,11 @@ import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api"
 
 const containerStyle = { width: "100%", height: "100svh" }
 
-const center = { lat: 30.2672, lng: -97.7431 }
+const mapOptions = {
+    center: { lat: 30.2672, lng: -97.7431 },
+    zoom: 11,
+    disableDefaultUI: true
+}
 
 const addresses = [
     "51 Rainey St #120, Austin, TX 78701",
@@ -43,7 +47,7 @@ const FindInStores = () => {
   if (!isLoaded) return <div>Loading Map...</div>
 
   return (
-    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={11}>
+    <GoogleMap mapContainerStyle={containerStyle} options={mapOptions}>
       {markers.map((pos, i) => (
         <Marker key={i} position={pos} />
       ))}
